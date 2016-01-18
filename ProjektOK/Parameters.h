1dad013889251e957d7d147b0f6d9769292633fb
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-01-18 13:23:48 +0100

    Change parameters

--- ProjektOK/Parameters.h
+++ ProjektOK/Parameters.h
@@ -4,12 +4,12 @@ namespace Parameters {
 	namespace Feromone_Table {
 		const int		INITIAL_VALUE		= 1;
 		const float		VAPORING			= 5;
-		const float		ADDING_BONUS		= 0.5;
+		const float		ADDING_BONUS		= 2;
 	}
 
 	namespace Ants {
 		const int		NUMBER				= 25;
-		const float		TABLE_PROBABILITY	= 1; // im wiêcej tym d³u¿ej random ma wiêksze szanse
+		const float		TABLE_PROBABILITY	= 1.5; // im wiêcej tym d³u¿ej random ma wiêksze szanse
 	}
 
 	namespace Tasks {
@@ -17,7 +17,7 @@ namespace Parameters {
 
 		namespace Length {
 			const int	MIN					= 50;
-			const int	MAX					= 500;
+			const int	MAX = 150;
 		}
 	}
 	namespace Iterations {
