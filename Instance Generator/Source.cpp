2950fec8507305f3ce0c4910d1bd8d3add858bd7
AkaG <kuba0095@gmail.com>
2016-01-22 21:58:17 +0100

    Generator v2

--- Instance Generator/Source.cpp
+++ Instance Generator/Source.cpp
@@ -10,11 +10,11 @@ using namespace std;
 int main() {
 	Instance* instance;
 
-	for (int i = 0;i < 5;i++) {
+	for (int i = 0;i < 10;i++) {
 		instance = new Instance(50+20*i, i);
 		instance->generateTasks(100, 150);
 		instance->generateBreaks();
-		instance->saveToFile(std::to_string(i));
+		instance->saveToFile(std::to_string(i).append(".txt"));
 		delete instance;
 	}
 	system("pause");
